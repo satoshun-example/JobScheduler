4a9a7050892ef3261342c78a37f7d4746eff23b5
SatoShun <shun.sato1@gmail.com>
2016-05-12 11:29:35 +0000

    publish Job

--- app/src/main/java/com/github/satoshun/example/jobscheduler/MainActivity.java
+++ app/src/main/java/com/github/satoshun/example/jobscheduler/MainActivity.java
@@ -1,13 +1,23 @@
 package com.github.satoshun.example.jobscheduler;
 
+import android.app.job.JobInfo;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static int jobId = 0;
+
     private ComponentName componentName;
+    private TestJobService testJobService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +30,30 @@ public class MainActivity extends AppCompatActivity {
     private void runJob() {
         componentName = new ComponentName(this, TestJobService.class);
         Intent intent = new Intent(this, TestJobService.class);
+        intent.putExtra("handler", new Messenger(new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                Log.d(TAG, "handleMessage");
+                testJobService = (TestJobService) msg.obj;
+                scheduleJob();
+            }
+        }));
         startService(intent);
+    }
+
+    private void scheduleJob() {
+        findViewById(R.id.root).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                scheduleJob();
+                if (testJobService == null) {
+                    return;
+                }
+
+                JobInfo.Builder builder = new JobInfo.Builder(jobId++, componentName);
+                builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY);
+                testJobService.scheduleJob(builder.build());
+            }
+        }, 5000);
     }
 }
--- app/src/main/java/com/github/satoshun/example/jobscheduler/TestJobService.java
+++ app/src/main/java/com/github/satoshun/example/jobscheduler/TestJobService.java
@@ -1,7 +1,14 @@
 package com.github.satoshun.example.jobscheduler;
 
+import android.app.job.JobInfo;
 import android.app.job.JobParameters;
+import android.app.job.JobScheduler;
 import android.app.job.JobService;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.util.Log;
 
 public class TestJobService extends JobService {
@@ -12,16 +19,42 @@ public class TestJobService extends JobService {
     public void onCreate() {
         super.onCreate();
 
-        Log.i(TAG, "Service created");
+        Log.i(TAG, "onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "onStartCommand");
+        if (intent == null) return super.onStartCommand(intent, flags, startId);
+
+        Messenger handler = intent.getParcelableExtra("handler");
+        Message message = Message.obtain();
+        message.obj = this;
+
+        try {
+            handler.send(message);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public boolean onStartJob(JobParameters params) {
-        return false;
+        Log.i(TAG, "onStartJob");
+        return true;
     }
 
     @Override
     public boolean onStopJob(JobParameters params) {
+        Log.i(TAG, "onStopJob");
         return false;
+    }
+
+    void scheduleJob(JobInfo t) {
+        Log.i(TAG, "scheduleJob");
+        JobScheduler tm = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        tm.schedule(t);
     }
 }
